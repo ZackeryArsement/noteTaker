bb9fbd52cbe8b56b3e62fe79b9b67a0ff5b7132b
Zackery Arsement <zarsement@hotmail.com>
2022-02-01 15:05:46 -0600

    Changed PORT value to enable Heroku use.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const readFromFile = util.promisify(fs.readFile);
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
