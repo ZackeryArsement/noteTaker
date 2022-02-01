0d50689460202b780b5ec80c8d24000abbfc4a89
Zackery Arsement <zarsement@hotmail.com>
2022-02-01 09:55:20 -0600

    Added get/post routes for /notes and /api/notes. Web page can now append a new note to the page. Still need to delete notes

--- Develop/server.js
+++ Develop/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
 const PORT = 3001;
 
 const app = express();
+
+app.use(express.json());
+
+app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
@@ -15,6 +22,31 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );  
+
+// GET Route for notes page
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);  
+
+// POST Route for a new UX/UI tip
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
+
+  const { title, tip } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      title,
+      tip,
+      id: uuid()
+    };
+
+    readAndAppend(newTip, './db/db.json');
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
 
 // Append data content to a given file in JSON format
 const readAndAppend = (content, file) => {
@@ -28,6 +60,11 @@ fs.readFile(file, 'utf8', (err, data) => {
     }
 });
 };
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
