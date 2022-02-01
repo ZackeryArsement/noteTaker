28340a9e0188bb9d8c35b05bcf2ca08e0d7dea48
Zackery Arsement <zarsement@hotmail.com>
2022-02-01 09:56:04 -0600

    Fixed error when selected save notes would not provide the note's text. Delete button still does not work.

--- Develop/server.js
+++ Develop/server.js
@@ -23,21 +23,26 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );  
 
-// GET Route for notes page
+// GET Route for notes json
 app.get('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/db/db.json'))
-);  
+);
 
-// POST Route for a new UX/UI tip
+// GET notes with id
+app.get('/api/notes:id', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+// POST Route for a new UX/UI note
 app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a tip`);
+  console.info(`${req.method} request received to add a note`);
 
-  const { title, tip } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newTip = {
       title,
-      tip,
+      text,
       id: uuid()
     };
 
